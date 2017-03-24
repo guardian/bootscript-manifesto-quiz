b8b287cdba6681230295d291a964f6993e962b05
Zef <zef.cherry-kynaston.casual@guardian.co.uk>
2017-03-24 17:23:44 +0000

    Changes by Tupac

--- src/js/main.js
+++ src/js/main.js
@@ -10,28 +10,30 @@ var comingSoonIndex = 0;
 
 export function init(dom, context, config, mediator) {
   var contentBody = document.querySelector('.content__article-body');
-
-
+  var headline = document.querySelector('.content__headline--immersive ');
+  if(headline) {
+      headline.remove();
+  }
   var parkourImageOne = document.createElement('figure');
   parkourImageOne.setAttribute('class', 'element--immersive parkour-image parkour-image-one');
-  parkourImageOne.innerHTML = '<div class="wrapper-one"><div class="person-one parkour-cutout-one"><img src="https://interactive.guim.co.uk/embed/article-embeds/parkour_02/hashed/rooftop-overlay.d23a30f2.png"></div><h2 class="parkour-text-one"><span>I fully</span><span>expected</span><span>him to die</span></h2></div>';
-  contentBody.insertBefore(parkourImageOne, contentBody.childNodes[10]);
+  parkourImageOne.innerHTML = '<div class="wrapper wrapper-one"><div class="person-one parkour-cutout-one"><img src="https://interactive.guim.co.uk/embed/article-embeds/parkour_01/hashed/rooftop-overlay.7205a2b7.png"></div><h2 class="parkour-text parkour-text-one"><span>He was</span><span>a firework,</span><span>a free spirit</span></h2></div><div class="caption"><span class="inline-information inline-icon reveal-caption-icon centered-icon rounded-icon"><svg width="6" height="14" viewBox="0 0 6 14" class="reveal-caption-icon__svg centered-icon__svg rounded-icon__svg inline-information__svg inline-icon__svg"><path d="M4.6 12l-.4 1.4c-.7.2-1.9.6-3 .6-.7 0-1.2-.2-1.2-.9 0-.2 0-.3.1-.5l2-6.7H.7l.4-1.5 4.2-.6h.2L3 12h1.6zm-.3-9.2c-.9 0-1.4-.5-1.4-1.3C2.9.5 3.7 0 4.6 0 5.4 0 6 .5 6 1.3c0 1-.8 1.5-1.7 1.5z"></path></svg></span> Nye Newman in La Chapelle des Buis in France, in April 2016. Photograph: Roxanne Valnet/ATB Parkour in Paris</div>';
+  contentBody.insertBefore(parkourImageOne, contentBody.childNodes[6]);
 
   var parkourImageTwo = document.createElement('figure');
   parkourImageTwo.setAttribute('class', 'element--immersive parkour-image parkour-image-two');
-  parkourImageTwo.innerHTML = '<div class="wrapper-two"><h2 class="parkour-text-two fadein"><span>I fully</span><span>expected</span><span>him to die</span></h2></div>';
+  parkourImageTwo.innerHTML = '<div class="wrapper wrapper-two"><h2 class="parkour-text parkour-text-two"><span>Most of us think,</span><span>I want to do that</span></h2></div><div class="caption"><span class="inline-information inline-icon reveal-caption-icon centered-icon rounded-icon"><svg width="6" height="14" viewBox="0 0 6 14" class="reveal-caption-icon__svg centered-icon__svg rounded-icon__svg inline-information__svg inline-icon__svg"><path d="M4.6 12l-.4 1.4c-.7.2-1.9.6-3 .6-.7 0-1.2-.2-1.2-.9 0-.2 0-.3.1-.5l2-6.7H.7l.4-1.5 4.2-.6h.2L3 12h1.6zm-.3-9.2c-.9 0-1.4-.5-1.4-1.3C2.9.5 3.7 0 4.6 0 5.4 0 6 .5 6 1.3c0 1-.8 1.5-1.7 1.5z"></path></svg></span> Nye Newman in Hong Kong, in August 2016. Photograph: Petar Cule</div>';
   contentBody.insertBefore(parkourImageTwo, contentBody.childNodes[20]);
 
   var parkourImageThree = document.createElement('figure');
   parkourImageThree.setAttribute('class', 'element--immersive parkour-image parkour-image-three');
-  parkourImageThree.innerHTML = '<div class="wrapper-three"><h2 class="parkour-text-three fadein"><span>I fully</span><span>expected</span><span>him to die</span></h2></div>';
+  parkourImageThree.innerHTML = '<div class="wrapper wrapper-three"><h2 class="parkour-text parkour-text-three"><span>He listens to</span><span>Mozart while</span><span>he climbs</span></h2></div><div class="caption"><span class="inline-information inline-icon reveal-caption-icon centered-icon rounded-icon"><svg width="6" height="14" viewBox="0 0 6 14" class="reveal-caption-icon__svg centered-icon__svg rounded-icon__svg inline-information__svg inline-icon__svg"><path d="M4.6 12l-.4 1.4c-.7.2-1.9.6-3 .6-.7 0-1.2-.2-1.2-.9 0-.2 0-.3.1-.5l2-6.7H.7l.4-1.5 4.2-.6h.2L3 12h1.6zm-.3-9.2c-.9 0-1.4-.5-1.4-1.3C2.9.5 3.7 0 4.6 0 5.4 0 6 .5 6 1.3c0 1-.8 1.5-1.7 1.5z"></path></svg></span> ‘Mustang Wanted’, a young Ukrainian whose videos have been watched 30m times. Photograph: Pete Kiehart for the Guardian</div>';
   contentBody.insertBefore(parkourImageThree, contentBody.childNodes[40]);
+
 
   var parkourImageFour = document.createElement('figure');
   parkourImageFour.setAttribute('class', 'element--immersive parkour-image parkour-image-four');
-  parkourImageFour.innerHTML = '<div class="wrapper-four"><div class="parkour-background-four"><img src="https://interactive.guim.co.uk/embed/article-embeds/parkour_04/hashed/rooftop.2029d8f9.jpg"></div><div class="person-four"><img src="https://interactive.guim.co.uk/embed/article-embeds/parkour_04/hashed/person.954c4b27.png"></div><h2 class="parkour-text-four"><span>I was born</span><span>on the edge</span><span>of a skyscraper</span></h2></div>';
-  contentBody.insertBefore(parkourImageFour, contentBody.childNodes[80]);
-
+  parkourImageFour.innerHTML = '<div class="wrapper wrapper-four"><div class="concrete"><img src="https://s3.amazonaws.com/gdn-cdn/embed/article-embeds/parkour_04/hashed/concrete.9c31e730.png"></div><div class="person-four parkour-cutout-four"><img src="https://s3.amazonaws.com/gdn-cdn/embed/article-embeds/parkour_04/hashed/person.1215cb5e.png"></div><h2 class="parkour-text parkour-text-four"><span>I was born</span><span>on the edge</span><span>of a skyscraper</span></h2></div><div class="caption"><span class="inline-information inline-icon reveal-caption-icon centered-icon rounded-icon"><svg width="6" height="14" viewBox="0 0 6 14" class="reveal-caption-icon__svg centered-icon__svg rounded-icon__svg inline-information__svg inline-icon__svg"><path d="M4.6 12l-.4 1.4c-.7.2-1.9.6-3 .6-.7 0-1.2-.2-1.2-.9 0-.2 0-.3.1-.5l2-6.7H.7l.4-1.5 4.2-.6h.2L3 12h1.6zm-.3-9.2c-.9 0-1.4-.5-1.4-1.3C2.9.5 3.7 0 4.6 0 5.4 0 6 .5 6 1.3c0 1-.8 1.5-1.7 1.5z"></path></svg></span> Mustang Wanted in Kiev in February. Photograph: Pete Kiehart for the Guardian</div>';
+  contentBody.insertBefore(parkourImageFour, contentBody.childNodes[72]);
 
 
 
@@ -51,20 +53,42 @@ export function init(dom, context, config, mediator) {
    var containerOffset =  container[0].getBoundingClientRect().top;
    var parrallax = containerOffset / 10;
    var text = document.querySelector('.parkour-text-one');
-   if(containerOffset <= 1){
+   if(containerOffset <= 200){
      text.classList.add("fadein");
    }
    block[0].style.bottom = parrallax * -2 + 'px' // move bubble1 at 20% of scroll rate
   }
 
+  function parkourTwo(){
+  //Parrallax animations
+  var container = document.getElementsByClassName('parkour-image-two');
+   var scrolltop = window.pageYOffset // get number of pixels document has scrolled vertically
+   var containerOffset =  container[0].getBoundingClientRect().top;
+   var text = document.querySelector('.parkour-text-two');
+   if(containerOffset <= 200){
+     text.classList.add("fadein");
+   }
+  }
+
+  function parkourThree(){
+  //Parrallax animations
+  var container = document.getElementsByClassName('parkour-image-three');
+   var scrolltop = window.pageYOffset // get number of pixels document has scrolled vertically
+   var containerOffset =  container[0].getBoundingClientRect().top;
+   var text = document.querySelector('.parkour-text-three');
+   if(containerOffset <= 200){
+     text.classList.add("fadein");
+   }
+  }
+
   function parkourFour(){
-   var blockFour = document.getElementsByClassName('parkour-background-four');
+   var blockFour = document.getElementsByClassName('parkour-cutout-four');
    var containerFour = document.getElementsByClassName('parkour-image-four');
    var scrolltop = window.pageYOffset // get number of pixels document has scrolled vertically
    var containerOffset =  containerFour[0].getBoundingClientRect().top;
    var parrallax = containerOffset / 10;
    var text = document.querySelector('.parkour-text-four');
-   if(containerOffset <= 1){
+   if(containerOffset <= 200){
      text.classList.add("fadein");
    }
    blockFour[0].style.bottom = parrallax * -2 + 'px' // move bubble1 at 20% of scroll rate
@@ -72,6 +96,8 @@ export function init(dom, context, config, mediator) {
 
   window.addEventListener('scroll', function(){ // on page scroll
    requestAnimationFrame(parkourOne) // call parallaxbubbles() on next available screen paint
+   requestAnimationFrame(parkourTwo)
+   requestAnimationFrame(parkourThree)
    requestAnimationFrame(parkourFour)
   }, false)
 }
