1fdba18fc898d675db00bce3e9390e2013fa0f79
Duarte Carrilho da Graça <duarte.carrilho.freelance@theguardian.com>
2017-06-06 13:44:13 +0100

    withdraws enhancement from app

--- src/js/main.js
+++ src/js/main.js
@@ -9,131 +9,138 @@ var headerContent;
 var submitButton;
 
 export function init(dom, context, config, mediator) {
-    var context = document;
-    var questions = context.querySelectorAll('.atom-quiz__question');
 
-    var headerContent = [
-        {
-            n: 1,
-            group: "Brexit,",
-            highlight: "#7D7569",
-            image: "https://uploads.guim.co.uk/2017/05/17/brexit.svg"
-        }, {
-            n: 2,
-            group: "Economy",
-            highlight: "#00456E",
-            image: "https://uploads.guim.co.uk/2017/05/17/economy.svg"
-        }, {
-            n: 3,
-            group: "Housing",
-            highlight: "#FFBB00",
-            image: "https://uploads.guim.co.uk/2017/05/26/housing.svg"
-        }, {
-            n: 4,
-            group: "Health",
-            highlight: "#CC2B12",
-            image: "https://uploads.guim.co.uk/2017/05/17/health.svg"
-        }, {
-            n: 5,
-            group: "Renationalisation",
-            highlight: '#951C55',
-            image: false
-        }, {
-            n: 6,
-            group: "Foreign and defence",
-            highlight: "#717D85",
-            image: "https://uploads.guim.co.uk/2017/05/26/defence.svg"
-        }, {
-            n: 7,
-            group: "Immigration",
-            highlight: "#B51800",
-            image: "https://uploads.guim.co.uk/2017/05/26/immigration.svg"
-        }, {
-            n: 8,
-            group: "Tax and spending",
-            highlight: "#1C6326",
-            image: "https://uploads.guim.co.uk/2017/05/26/tax.svg"
-        }, {
-            n: 9,
-            group: "Education",
-            highlight: "#197CAA",
-            image: "https://uploads.guim.co.uk/2017/05/26/education.svg"
-        }, {
-            n: 10,
-            group: "Secondary School",
-            highlight: "#4BC6DF",
-            image: false
-        }, {
-            n: 11,
-            group: "Environment",
-            highlight: "#5EBFBA",
-            image: "https://uploads.guim.co.uk/2017/05/26/environment.svg"
-        }, {
-            n: 12,
-            group: "Workers rights",
-            highlight: "#507892",
-            image: "https://uploads.guim.co.uk/2017/06/05/workers-rights.svg"
+    if (document.querySelectorAll('body.ios, body.android, body.windows').length==0) {
+
+        var context = document;
+        var questions = context.querySelectorAll('.atom-quiz__question');
+
+        var headerContent = [
+            {
+                n: 1,
+                group: "Brexit,",
+                highlight: "#7D7569",
+                image: "https://uploads.guim.co.uk/2017/05/17/brexit.svg"
+            }, {
+                n: 2,
+                group: "Economy",
+                highlight: "#00456E",
+                image: "https://uploads.guim.co.uk/2017/05/17/economy.svg"
+            }, {
+                n: 3,
+                group: "Housing",
+                highlight: "#FFBB00",
+                image: "https://uploads.guim.co.uk/2017/05/26/housing.svg"
+            }, {
+                n: 4,
+                group: "Health",
+                highlight: "#CC2B12",
+                image: "https://uploads.guim.co.uk/2017/05/17/health.svg"
+            }, {
+                n: 5,
+                group: "Renationalisation",
+                highlight: '#951C55',
+                image: false
+            }, {
+                n: 6,
+                group: "Foreign and defence",
+                highlight: "#717D85",
+                image: "https://uploads.guim.co.uk/2017/05/26/defence.svg"
+            }, {
+                n: 7,
+                group: "Immigration",
+                highlight: "#B51800",
+                image: "https://uploads.guim.co.uk/2017/05/26/immigration.svg"
+            }, {
+                n: 8,
+                group: "Tax and spending",
+                highlight: "#1C6326",
+                image: "https://uploads.guim.co.uk/2017/05/26/tax.svg"
+            }, {
+                n: 9,
+                group: "Education",
+                highlight: "#197CAA",
+                image: "https://uploads.guim.co.uk/2017/05/26/education.svg"
+            }, {
+                n: 10,
+                group: "Secondary School",
+                highlight: "#4BC6DF",
+                image: false
+            }, {
+                n: 11,
+                group: "Environment",
+                highlight: "#5EBFBA",
+                image: "https://uploads.guim.co.uk/2017/05/26/environment.svg"
+            }, {
+                n: 12,
+                group: "Workers rights",
+                highlight: "#507892",
+                image: "https://uploads.guim.co.uk/2017/06/05/workers-rights.svg"
+            }
+        ];
+
+
+        for (var i = 0, len = questions.length; i < len; i++) {
+            var question = questions[i];
+            var d = headerContent[i];
+
+            var questionText = question.querySelector('.atom-quiz__question-text');
+            var questionAnswers = question.querySelector('.atom-quiz__answers');
+
+            var header = document.createElement("div");
+            header.setAttribute('class', 'question-header');
+
+            var graphic = document.createElement("div");
+            graphic.setAttribute('class', 'graphic');
+
+            if (d['image']) {
+                var image = document.createElement("img");
+                image.setAttribute('class', 'illustration');
+                image.setAttribute('src', d['image']);
+                graphic.appendChild(image);
+            }
+
+            var bars = document.createElement('div');
+            bars.setAttribute('class', 'bars');
+            bars.innerHTML = '<div class="bar bar-1" style="background-color:'+d['highlight']+'"></div><div class="bar bar-2" style="background-color:'+d['highlight']+'"></div><div class="bar bar-3" style="background-color:'+d['highlight']+'"></div><div class="bar bar-4" style="background-color:'+d['highlight']+'"></div>';
+            graphic.appendChild(bars);
+
+
+            header.appendChild(graphic);
+            question.insertBefore(header, questionAnswers);
+            header.appendChild(questionText);
         }
-    ];
 
+        submitButton = document.createElement('div');
+        submitButton.setAttribute('id', 'submit-answers');
+        submitButton.innerText = 'Submit answers';
+        var quizAtom = document.querySelector('.element-atom');
+        quizAtom.appendChild(submitButton);
 
-    for (var i = 0, len = questions.length; i < len; i++) {
-        var question = questions[i];
-        var d = headerContent[i];
+        submitButton.addEventListener('click', function() {
+            if (submitButton.classList.contains('active')) {
+                scoreQuiz();
+                submitButton.classList.add('hidden')
+            }
+        });
 
-        var questionText = question.querySelector('.atom-quiz__question-text');
-        var questionAnswers = question.querySelector('.atom-quiz__answers');
-
-        var header = document.createElement("div");
-        header.setAttribute('class', 'question-header');
-
-        var graphic = document.createElement("div");
-        graphic.setAttribute('class', 'graphic');
-
-        if (d['image']) {
-            var image = document.createElement("img");
-            image.setAttribute('class', 'illustration');
-            image.setAttribute('src', d['image']);
-            graphic.appendChild(image);
+        var labels = document.querySelectorAll('.element-atom label');
+        for (var j = 0; j < labels.length; j++) {
+            labels[j].addEventListener('click', function() {
+                setTimeout(function() {
+                    updateSubmitButton();
+                }, 100);
+            })
         }
 
-        var bars = document.createElement('div');
-        bars.setAttribute('class', 'bars');
-        bars.innerHTML = '<div class="bar bar-1" style="background-color:'+d['highlight']+'"></div><div class="bar bar-2" style="background-color:'+d['highlight']+'"></div><div class="bar bar-3" style="background-color:'+d['highlight']+'"></div><div class="bar bar-4" style="background-color:'+d['highlight']+'"></div>';
-        graphic.appendChild(bars);
+
+        setTimeout(function() {
+            updateSubmitButton();
+        }, 1000);
 
 
-        header.appendChild(graphic);
-        question.insertBefore(header, questionAnswers);
-        header.appendChild(questionText);
     }
 
-    submitButton = document.createElement('div');
-    submitButton.setAttribute('id', 'submit-answers');
-    submitButton.innerText = 'Submit answers';
-    var quizAtom = document.querySelector('.element-atom');
-    quizAtom.appendChild(submitButton);
-
-    submitButton.addEventListener('click', function() {
-        if (submitButton.classList.contains('active')) {
-            scoreQuiz();
-            submitButton.classList.add('hidden')
-        }
-    });
-
-    var labels = document.querySelectorAll('.element-atom label');
-    for (var j = 0; j < labels.length; j++) {
-        labels[j].addEventListener('click', function() {
-            setTimeout(function() {
-                updateSubmitButton();
-            }, 100);
-        })
-    }
-
-
-    setTimeout(function() {
-        updateSubmitButton();
-    }, 1000);
 }
 
 
